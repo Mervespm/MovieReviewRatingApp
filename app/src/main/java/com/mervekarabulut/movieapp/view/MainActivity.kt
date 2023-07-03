76e523a598a997be345246577a03d33dd2f84473
Merve Karabulut <mrvkrblt14@gmail.com>
2023-07-03 14:37:57 +0300

    Starting application, main, login and register activity done

---
+++ app/src/main/java/com/mervekarabulut/movieapp/view/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.mervekarabulut.movieapp.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mervekarabulut.movieapp.R
+import com.mervekarabulut.movieapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/mervekarabulut/movieapp/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.mervekarabulut.movieapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mervekarabulut.movieapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
