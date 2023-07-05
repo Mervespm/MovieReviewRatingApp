33d17e2ab739ef588e4bd9775166d35a7f453925
Merve Karabulut <mrvkrblt14@gmail.com>
2023-07-06 02:02:05 +0300

    Comment partially implemented

--- app/src/main/java/com/mervekarabulut/movieapp/view/MovieDetailsActivity.kt
+++ app/src/main/java/com/mervekarabulut/movieapp/view/MovieDetailsActivity.kt
@@ -2,19 +2,24 @@ package com.mervekarabulut.movieapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FirebaseFirestore
 import com.mervekarabulut.movieapp.R
 import com.mervekarabulut.movieapp.databinding.ActivityMovieDetailsBinding
 import com.mervekarabulut.movieapp.model.Movie
 
 class MovieDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMovieDetailsBinding
-
+    private lateinit var firestore: FirebaseFirestore
+    private var ratingInt : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMovieDetailsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        firestore = FirebaseFirestore.getInstance()
 
         val movie = intent.getSerializableExtra("movie") as? Movie
         if (movie != null) {
@@ -29,5 +34,37 @@ class MovieDetailsActivity : AppCompatActivity() {
                 .placeholder(R.drawable.placeholder_image)
                 .into(binding.moviePosterImageView)
         }
+
+        val rBar = findViewById<RatingBar>(R.id.ratingBar)
+        rBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            // Retrieve the rating value
+            ratingInt = rating.toInt()
+            // You can use the selectedRating value as needed
+        }
+
+        binding.btnSubmit.setOnClickListener {
+            saveCommentToFirestore()
+        }
+
+    }
+    private fun saveCommentToFirestore() {
+
+        val rating = ratingInt
+        val comment = binding.commentEditText.text.toString()
+
+        // Create a new comment document in Firestore
+        val commentData = hashMapOf(
+            "comment" to comment,
+            "rating" to rating
+        )
+
+        firestore.collection("Comments")
+            .add(commentData)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Comment saved successfully", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, "Failed to save comment: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/mervekarabulut/movieapp/view/UserProfileActivity.kt
+++ app/src/main/java/com/mervekarabulut/movieapp/view/UserProfileActivity.kt
@@ -1,30 +1,107 @@
 package com.mervekarabulut.movieapp.view
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
 import com.mervekarabulut.movieapp.databinding.ActivityUserProfileBinding
 
 class UserProfileActivity : AppCompatActivity() {
-
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var storageReference: StorageReference
     private lateinit var binding : ActivityUserProfileBinding
+
+    private var selectedImageUri: Uri? = null
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        uri?.let {
+            selectedImageUri = it
+            binding.imageView.setImageURI(selectedImageUri)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
 
         super.onCreate(savedInstanceState)
         binding = ActivityUserProfileBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        loadProfileDetails()
+
+        binding.imageView.setOnClickListener {
+            openGallery()
+        }
 
         binding.btnLogout.setOnClickListener {
             Firebase.auth.signOut()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
     }
+    private fun loadProfileDetails() {
+        val userId = auth.currentUser?.uid
+
+        if (userId != null) {
+            val userDocument = firestore.collection("Users").document(userId)
+            userDocument.get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        val fullName = documentSnapshot.getString("fullName")
+                        binding.textView.text = fullName
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    // Handle the failure case
+                }
+        }
+    }
+
+    private fun openGallery() {
+        getContent.launch("image/*")
+    }
+
+
+    private fun uploadProfileImage() {
+        val userId = auth.currentUser?.uid
+        userId?.let { uid ->
+            selectedImageUri?.let { imageUri ->
+                val profileImageRef = storageReference.child("profile_images/$uid")
+                profileImageRef.putFile(imageUri)
+                    .addOnSuccessListener { taskSnapshot ->
+                        // Image upload success
+                        profileImageRef.downloadUrl.addOnSuccessListener { uri ->
+                            val profileImageUrl = uri.toString()
+                            // Save the profileImageUrl to the user's document in Firestore
+                            val userDocumentRef = firestore.collection("Users").document(uid)
+                            userDocumentRef.update("profileImageUrl", profileImageUrl)
+                                .addOnSuccessListener {
+                                    // Profile image URL saved successfully
+                                }
+                                .addOnFailureListener { exception ->
+                                    // Handle the failure case
+                                }
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        // Handle image upload failure
+                    }
+            }
+        }
+    }
+
 
 
 
